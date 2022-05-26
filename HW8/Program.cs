089e7c5945368f82df1e9e580f0f741ddffe8197
jonsi <jonsi@DESKTOP-VI5FVR6>
2022-05-26 20:22:18 +0400

    Добавьте файлы проекта.

---
+++ HW8/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к папке");
+        string DirName = Console.ReadLine();
+        DirDelet(DirName);
+    }
+
+    static void DirDelet(string dirName)
+    {
+        try
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+
+            if (dirInfo.Exists)
+            {
+                DirectoryInfo[] folderNames = dirInfo.GetDirectories();
+
+                foreach (var item in folderNames)
+                {
+                    DirDelet(item.FullName);
+
+                    if (DateTime.Now - item.LastAccessTime > TimeSpan.FromMinutes(30))
+                    {
+                        item.Delete(true);
+                    }
+                }
+
+                FileInfo[] fileNames = dirInfo.GetFiles();
+
+                foreach (var item in fileNames)
+                {
+                    if (DateTime.Now - item.LastAccessTime > TimeSpan.FromMinutes(30))
+                    {
+                        item.Delete();
+                    }
+                }
+
+                Console.WriteLine($"Очистка {dirInfo.FullName} от файлов и папок, не использующихся более 30 минут завершена");
+            }
+            else
+            {
+                Console.WriteLine($"Не верный адрес дериктории");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+}
